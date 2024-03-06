9772a1cde9a53bc078d9407c7952db51a0107f60
Prasenjit Das <prasun.das.89@gmail.com>
2024-03-05 18:40:30 -0600

    Add eslint-disable for unused vars and allow JS in tsconfig.app.json

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable @typescript-eslint/no-unused-vars -->
 <script setup lang="ts">
 import WelcomeItem from './WelcomeItem.vue'
 import DocumentationIcon from './icons/IconDocumentation.vue'
