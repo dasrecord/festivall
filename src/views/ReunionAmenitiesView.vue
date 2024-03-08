a94f426328ecf4d862ab9818c3a108df04cf3eb2
Prasenjit Das <prasun.das.89@gmail.com>
2024-03-07 18:10:37 -0600

    Fix formatting of links in Reunion views

--- src/views/ReunionAmenitiesView.vue
+++ src/views/ReunionAmenitiesView.vue
@@ -31,11 +31,11 @@ export default {
     </h2>
     <h1>
       Want to perform or volunteer?‍<br />
-      <a href="https://dasrecord.typeform.com/to/a65mFuLr">APPLY NOW </a>
+      <a href="https://dasrecord.typeform.com/to/a65mFuLr">APPLY NOW</a>
     </h1>
     <h1>
       Ready to buy tickets?‍<br />
-      <a href="https://dasrecord.typeform.com/to/dyJs6iFd"> BUY TICKETS </a>
+      <a href="https://dasrecord.typeform.com/to/dyJs6iFd">BUY TICKETS</a>
     </h1>
   </div>
 </template>
--- src/views/ReunionTeamView.vue
+++ src/views/ReunionTeamView.vue
@@ -160,11 +160,11 @@ export default {
     </div> 
     <h1>
       Want to perform or volunteer?‍<br />
-      <a href="https://dasrecord.typeform.com/to/a65mFuLr">APPLY NOW </a>
+      <a href="https://dasrecord.typeform.com/to/a65mFuLr">APPLY NOW</a>
     </h1>
     <h1>
       Ready to buy tickets?‍<br />
-      <a href="https://dasrecord.typeform.com/to/dyJs6iFd"> BUY TICKETS </a>
+      <a href="https://dasrecord.typeform.com/to/dyJs6iFd">BUY TICKETS</a>
     </h1>
   </div>
 </template>
