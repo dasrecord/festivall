aec5797e41ed332abaef2a67a9d251d88b161a64
Prasenjit Das <prasun.das.89@gmail.com>
2024-04-01 20:17:19 -0600

    Remove unused icons in TheWelcome.vue

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,11 +1,6 @@
 <!-- eslint-disable @typescript-eslint/no-unused-vars -->
 <script setup lang="ts">
 import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
 import IconFestivall from './icons/IconFestivall.vue'
 </script>
 
