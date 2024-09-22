1178a1fe0bf2ad752f030ac33883e7787b44477f
Prasenjit Das <prasun.das.89@gmail.com>
2024-09-21 22:38:58 -0600

    refactor: Remove deprecated images in ReunionFamilyView component for 2025 refresh

--- src/views/ReunionFamilyView.vue
+++ src/views/ReunionFamilyView.vue
@@ -1,21 +1,15 @@
 <script>
 import CountdownTimer from '@/components/CountdownTimer.vue'
 import CarouselComponent from '@/components/carousel/CarouselComponent.vue'
-import arrow_image from '@/assets/images/arrow.jpg'
-import audience_image from '@/assets/images/audience.jpg'
-import costume_image from '@/assets/images/costume.jpg'
-import friends_image from '@/assets/images/friends.jpg'
-import grooving_image from '@/assets/images/grooving.jpg'
+
 import janicka_image from '@/assets/images/janicka.jpg'
 import jordan_image from '@/assets/images/jordan.jpg'
 import kevin_image from '@/assets/images/kevin.jpg'
-import kids_image from '@/assets/images/kids.jpg'
 import kirk_image from '@/assets/images/kirk.jpg'
 import night_image from '@/assets/images/night.jpg'
-import perform_image from '@/assets/images/perform.jpg'
 import talk_image from '@/assets/images/talk.jpg'
-import tribal_image from '@/assets/images/tribal.jpg'
 import vandamage_image from '@/assets/images/vandamage.jpg'
+
 import DetailsPanel from '@/components/DetailsPanel.vue'
 import CalltoAction from '@/components/CalltoAction.vue'
 
@@ -28,20 +22,12 @@ export default {
   },
   data: () => ({
     slides: [
-      arrow_image,
-      audience_image,
-      costume_image,
-      friends_image,
-      grooving_image,
       janicka_image,
       jordan_image,
       kevin_image,
-      kids_image,
       kirk_image,
       night_image,
-      perform_image,
       talk_image,
-      tribal_image,
       vandamage_image
     ]
   })
