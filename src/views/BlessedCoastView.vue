79e290b06db3e818a3622f0230218525f20e7366
Prasenjit Das <prasun.das.89@gmail.com>
2024-09-22 10:38:27 -0600

    refactor: Update BlessedCoastView component styling and content
    - Update image source in BlessedCoastView component
    - Remove heading and text content in BlessedCoastView component
    - Adjust styling of the image in BlessedCoastView component

--- src/views/BlessedCoastView.vue
+++ src/views/BlessedCoastView.vue
@@ -1,5 +1,5 @@
 <script>
-import wolf_image from '@/assets/images/wolf2.jpg'
+import wolf_image from '@/assets/images/blessed_coast_coming_soon.jpg'
 import BlessedCoastCalltoAction from '@/components/BlessedCoastCalltoAction.vue'
 
 export default {
@@ -15,8 +15,6 @@ export default {
 </script>
 <template>
   <div class="basic">
-    <h1>THE BLESSED COAST <br /></h1>
-    Coming soon...<br />
     <img class="wolf" :src="wolfImage" alt="wolf" />
     <BlessedCoastCalltoAction />
   </div>
@@ -36,9 +34,7 @@ h1 {
 }
 img {
   width: 100%;
-  height: auto;
   border-radius: 25px;
-  margin-top: 1rem;
 }
 a {
   display: inline-block;
