b940ecb4811dc827e3674f739bb9260012cf96f6
Prasenjit Das <prasun.das.89@gmail.com>
2024-09-26 22:17:22 -0600

    update fb video styling

--- src/views/BlessedCoastView.vue
+++ src/views/BlessedCoastView.vue
@@ -18,26 +18,16 @@ export default {
     <img class="wolf" :src="wolfImage" alt="wolf" />
     <div class="video">
       <div class="video-wrapper">
-        <!-- <iframe
-          width="100%"
-          height="300"
-          src="https://www.youtube.com/embed/x7OlE-qvZms?si=zXu__l3U05-20ODD"
-          title="Blessed Coast Trailer"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerpolicy="strict-origin-when-cross-origin"
-          allowfullscreen
-        ></iframe> -->
-        <iframe
-          src="https://www.facebook.com/plugins/video.php?height=295&href=https%3A%2F%2Fwww.facebook.com%2Fblessedfestivalbc%2Fvideos%2F862716683897140%2F&show_text=false&width=560&t=0"
-          width="100%"
-          height="500"
-          style="border: none; overflow: hidden"
-          scrolling="no"
-          frameborder="0"
-          allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-          allowFullScreen
-        ></iframe>
+        <div class="iframe-container">
+          <iframe
+            src="https://www.facebook.com/plugins/video.php?height=295&href=https%3A%2F%2Fwww.facebook.com%2Fblessedfestivalbc%2Fvideos%2F862716683897140%2F&show_text=false&width=560&t=0"
+            title="Blessed Coast Trailer"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
+        </div>
       </div>
     </div>
     <BlessedCoastCalltoAction />
@@ -56,22 +46,22 @@ img {
 }
 .video {
   width: 100%;
-  border-radius: 25px;
+
   position: relative;
   z-index: 1;
 }
 .video-wrapper {
   overflow: hidden;
-  border-radius: 25px;
   position: relative;
   z-index: 1;
   margin-top: 1rem;
 }
-.video-wrapper iframe {
-  display: block;
+.iframe-container {
+  overflow: hidden;
+}
+.iframe-container iframe {
   width: 100%;
-  position: relative;
-  z-index: 1;
-  pointer-events: auto;
+  height: 450px;
+  border-radius: 25px;
 }
 </style>
