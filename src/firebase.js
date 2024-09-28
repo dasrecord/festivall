1f85b728c647c9d8f5f6ed520a38b23b56f4c0ff
Prasenjit Das <prasun.das.89@gmail.com>
2024-09-27 19:14:11 -0600

    Update Firebase configuration for Reunion and Festivall apps AND update github repository secrets

--- src/firebase.js
+++ src/firebase.js
@@ -3,23 +3,23 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const ReunionConfig = {
-  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: import.meta.env.VITE_APP_REUNION_API_KEY,
+  authDomain: import.meta.env.VITE_APP_REUNION_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_REUNION_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_REUNION_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_REUNION_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_REUNION_APP_ID,
+  measurementId: import.meta.env.VITE_APP_REUNION_MEASUREMENT_ID
 };
 
 const FestivallConfig = {
-  apiKey: import.meta.env.VITE_APP_AUTH_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_APP_AUTH_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_APP_AUTH_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_APP_AUTH_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_APP_AUTH_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_AUTH_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_APP_AUTH_FIREBASE_MEASUREMENT_ID
+  apiKey: import.meta.env.VITE_APP_FESTIVALL_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FESTIVALL_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_FESTIVALL_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FESTIVALL_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FESTIVALL_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_FESTIVALL_APP_ID,
+  measurementId: import.meta.env.VITE_APP_FESTIVALL_MEASUREMENT_ID
 };
 
 const reunion_app = initializeApp(ReunionConfig, 'reunion');
