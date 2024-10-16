c317992c71a9068127373d72e806d3e1cda1a669
Prasenjit Das <prasun.das.89@gmail.com>
2024-10-16 16:56:39 -0600

    updated styling

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -5,83 +5,100 @@ import IconFestivall from './icons/IconFestivall.vue'
 </script>
 
 <template>
-  <WelcomeItem>
-    <template #icon>
-      <IconFestivall />
-    </template>
-    <template #heading><RouterLink to="/reunion">The Reunion</RouterLink></template>
-    <p>
-      The Reunion is a music festival that takes place in the heart of the Canadian prairies<br />
-      featuring some of the best electronic music DJs and local acts.
-    </p>
-  </WelcomeItem>
-  <WelcomeItem>
-    <template #icon>
-      <IconFestivall />
-    </template>
-    <template #heading><RouterLink to="/fusecollective">Fuse Collective</RouterLink></template>
-    <p>
-      Fuse Collective is a group of artists, musicians, producers, DJ's, production specialists, and
-      promoters<br />
-      all coming together to create and support new growth in the scene.
-    </p>
-  </WelcomeItem>
+  <div class="container">
+    <WelcomeItem>
+      <template #icon>
+        <IconFestivall />
+      </template>
+      <template #heading><RouterLink to="/reunion">The Reunion</RouterLink></template>
+      <template #description>
+        The Reunion is a music festival that takes place in the heart of the Canadian prairies<br />
+        featuring some of the best electronic music DJs and local acts.
+      </template>
+    </WelcomeItem>
+    <WelcomeItem>
+      <template #icon>
+        <IconFestivall />
+      </template>
+      <template #heading><RouterLink to="/fusecollective">Fuse Collective</RouterLink></template>
+      <template #description>
+        Fuse Collective is a group of artists, musicians, producers, DJ's, production specialists,
+        and promoters<br />
+        all coming together to create and support new growth in the scene.
+      </template>
+    </WelcomeItem>
 
-  <WelcomeItem>
-    <template #icon>
-      <IconFestivall />
-    </template>
-    <template #heading><RouterLink to="/blessedcoast">Blessed Coast</RouterLink></template>
+    <WelcomeItem>
+      <template #icon>
+        <IconFestivall />
+      </template>
+      <template #heading><RouterLink to="/blessedcoast">Blessed Coast</RouterLink></template>
 
-    <p>
-      Blessed is a Celebration of Coastal Arts and Culture taking place in Squamish, BC.<br />
-      Multi-cultural, family friendly, music, yoga with inspired artists, teachers & workshops.
-    </p>
-  </WelcomeItem>
+      <template #description>
+        Blessed is a Celebration of Coastal Arts and Culture taking place in Squamish, BC.<br />
+        Multi-cultural, family friendly, music, yoga with inspired artists, teachers & workshops.
+      </template>
+    </WelcomeItem>
 
-  <WelcomeItem>
-    <template #icon>
-      <IconFestivall />
-    </template>
-    <template #heading
-      ><a href="https://www.instagram.com/impactfestival_bc/">Impact Festival</a></template
-    >
-    <p>
-      Showcasing the best western Canadian talent, Impact Music Festival based out of Vancouver,<br />is
-      a must-see event for any electronic music fan.
-    </p>
-  </WelcomeItem>
+    <WelcomeItem>
+      <template #icon>
+        <IconFestivall />
+      </template>
+      <template #heading
+        ><a href="https://www.instagram.com/impactfestival_bc/">Impact Festival</a></template
+      >
+      <template #description>
+        Showcasing the best western Canadian talent, Impact Music Festival based out of
+        Vancouver,<br />is a must-see event for any electronic music fan.
+      </template>
+    </WelcomeItem>
 
-  <WelcomeItem>
-    <template #icon>
-      <IconFestivall />
-    </template>
-    <template #heading
-      ><RouterLink to="/synergistic">Synergystic Entertainment</RouterLink></template
-    >
-    <p>A production company that specializes in electronic music events and festivals.</p>
-  </WelcomeItem>
+    <WelcomeItem>
+      <template #icon>
+        <IconFestivall />
+      </template>
+      <template #heading
+        ><RouterLink to="/synergistic">Synergystic Entertainment</RouterLink></template
+      >
+      <template #description
+        >A production company that specializes in electronic music events and festivals.</template
+      >
+    </WelcomeItem>
 
-  <WelcomeItem>
-    <template #icon>
-      <IconFestivall />
-    </template>
-    <template #heading><RouterLink to="/dasrecord">Das Record</RouterLink></template>
-    <p>
-      A medical doctor by day and a DJ/musician/coder by night,<br />
-      Das Record is a live piano and electronic music act and runs music festivals accross Canada.
-    </p>
-  </WelcomeItem>
-  <WelcomeItem>
-    <template #icon>
-      <IconFestivall />
-    </template>
-    <template #heading>
-      <a href="https://evolvedmusicgroup.com">Evolved Music Group</a>
-    </template>
-    <p>
-      An international artist management agency and music firm<br />
-      with A&R and publishing consultants with personel in Canada, the US, and Europe.
-    </p>
-  </WelcomeItem>
+    <WelcomeItem>
+      <template #icon>
+        <IconFestivall />
+      </template>
+      <template #heading><RouterLink to="/dasrecord">Das Record</RouterLink></template>
+      <template #description>
+        A medical doctor by day and a DJ/musician/coder by night,<br />
+        Das Record is a live piano and electronic music act and runs music festivals accross Canada.
+      </template>
+    </WelcomeItem>
+    <WelcomeItem>
+      <template #icon>
+        <IconFestivall />
+      </template>
+      <template #heading>
+        <a href="https://evolvedmusicgroup.com">Evolved Music Group</a>
+      </template>
+      <template #description>
+        An international artist management agency and music firm<br />
+        with A&R and publishing consultants with personel in Canada, the US, and Europe.
+      </template>
+    </WelcomeItem>
+  </div>
 </template>
+
+<style scoped></style>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container {
+}
+</style>
