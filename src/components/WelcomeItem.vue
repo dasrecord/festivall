41e1eea02bf2784f2f6647aab5ab4d25aa9fed05
Prasenjit Das <prasun.das.89@gmail.com>
2024-10-16 16:56:39 -0600

    updated styling

--- src/components/WelcomeItem.vue
+++ src/components/WelcomeItem.vue
@@ -7,8 +7,8 @@
       <h3>
         <slot name="heading"></slot>
       </h3>
-      <slot></slot>
     </div>
+    <slot name="description"></slot>
   </div>
 </template>
 
@@ -16,21 +16,23 @@
 .item {
   margin-top: 2rem;
   display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
   position: relative;
 }
 
 .details {
   flex: 1;
-  margin-left: 1rem;
 }
 
 i {
   display: flex;
   place-items: center;
   place-content: center;
-  width: 32px;
-  height: 32px;
-
+  width: 24px;
+  height: 24px;
   color: var(--color-text);
 }
 
@@ -44,6 +46,10 @@ h3 {
 @media (min-width: 1024px) {
   .item {
     margin-top: 0;
+    flex-direction: column;
+    align-items: flex-start;
+    flex-wrap: wrap;
+    text-align: left;
     padding-left: 1rem;
     padding-bottom: 2rem;
   }
@@ -52,7 +58,6 @@ h3 {
     top: calc(50% - 25px);
     left: -26px;
     position: absolute;
-    border: 1px solid var(--color-border);
     background: var(--color-background);
     border-radius: 8px;
     width: 24px;
