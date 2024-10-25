cf1609c548ecaec510db944b8673db2a5443d0f8
Prasenjit Das <prasun.das.89@gmail.com>
2024-10-24 23:31:29 -0600

    Refactor calendar file paths and update styling for lineup view

--- src/views/BlessedCoastLineupView.vue
+++ src/views/BlessedCoastLineupView.vue
@@ -59,30 +59,30 @@
       <h1>Yoga Temple</h1>
       <p>Friday</p>
       <CalendarModule
-        filePath="/data/calendars/blessed_coast_yoga-temple_calendar.ics"
+        filePath="/data/calendars/blessed_coast_yoga_temple_calendar.ics"
         :startDate="new Date('2024-08-01T14:00:00')"
         :endDate="new Date('2024-08-02T02:00:00')"
       />
       <p>Saturday</p>
       <CalendarModule
-        filePath="/data/calendars/blessed_coast_yoga-temple_calendar.ics"
+        filePath="/data/calendars/blessed_coast_yoga_temple_calendar.ics"
         :startDate="new Date('2024-08-02T14:00:00')"
         :endDate="new Date('2024-09-03T02:00:00')"
       />
       <p>Sunday</p>
       <CalendarModule
-        filePath="/data/calendars/blessed_coast_yoga-temple_calendar.ics"
+        filePath="/data/calendars/blessed_coast_yoga_temple_calendar.ics"
         :startDate="new Date('2024-08-03T14:00')"
         :endDate="new Date('2024-08-04T02:00')"
       />
       <p>Monday</p>
       <CalendarModule
-        filePath="/data/calendars/blessed_coast_yoga-temple_calendar.ics"
+        filePath="/data/calendars/blessed_coast_yoga_temple_calendar.ics"
         :startDate="new Date('2024-08-04T14:00')"
         :endDate="new Date('2024-08-05T02:00')"
       />
     </div>
-    <div class="workshop_space">
+    <div class="workshop-space">
       <h1>Workshop Space</h1>
       <p>Friday</p>
       <CalendarModule
@@ -119,7 +119,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 <style scoped>
 * {
-  border: 1px solid lime;
+  /* border: 1px solid lime; */
 }
 .basic {
   display: flex;
@@ -128,5 +128,18 @@ import HelloWorld from '@/components/HelloWorld.vue'
 }
 p {
   font-weight: bold;
+}
+.totem-stage {
+  background-color: #5484ed;
+}
+
+.dojo-stage {
+  background-color: #ffb878;
+}
+.yoga-temple {
+  background-color: #dbadff;
+}
+.workshop-space {
+  background-color: #fbd75b;
 }
 </style>
