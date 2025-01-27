b5162b86c6302bffe8f164b525fc2553504e0a73
Prasenjit Das <prasun.das.89@gmail.com>
2025-01-26 18:21:49 -0600

    fix: update API endpoint in PlaceHolderView for festival submissions

--- src/views/PlaceHolderView.vue
+++ src/views/PlaceHolderView.vue
@@ -23,7 +23,7 @@ const currentStep = ref(2)
 const submitForm = async () => {
   try {
     const response = await axios.post(
-      'https://relayproxy.vercel.app/reunion_slack',
+      'https://relayproxy.vercel.app/festivall_placeholder',
       {
         text: `Name: ${form.value.name}\nEmail: ${form.value.email}\nApplicant Type: ${form.value.applicant_type}\nAct Type: ${form.value.act_type}\nAct Name: ${form.value.act_name}\nAct Description: ${form.value.act_description}\nTrack/Mix URL: ${form.value.track_mix_url}\nAct Website: ${form.value.act_website}\nSocial Media URL: ${form.value.social_url}\nMessage: ${form.value.message}`
       },
