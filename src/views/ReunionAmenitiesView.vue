f4fdceeb9ea073afc79f36b5221c1dddfa17efc0
Prasenjit Das <prasun.das.89@gmail.com>
2025-02-18 13:23:16 -0600

    remove unnecessary lines

--- src/views/ReunionAmenitiesView.vue
+++ src/views/ReunionAmenitiesView.vue
@@ -160,10 +160,9 @@ export default {
   display: flex;
   align-items: center;
   gap: 0.5rem;
-  /* background: #f9f9f9; */
   padding: 0.5rem 1rem;
   border-radius: 8px;
-  /* box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); */
+
 }
 
 .amenity-icon {
