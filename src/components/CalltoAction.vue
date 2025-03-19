827d59b444f2b7a63917ec387a24f72299fcf19a
Prasenjit Das <prasun.das.89@gmail.com>
2025-03-19 09:24:25 -0600

    make entire call-to-action area clickable

--- src/components/CalltoAction.vue
+++ src/components/CalltoAction.vue
@@ -8,44 +8,55 @@ export default {
 
 <template>
   <div class="call-to-action">
-    <h1 class="volunteer">
-      Want to volunteer?‍<br />
-      <RouterLink to="/reunionapplication">APPLY NOW</RouterLink>
-    </h1>
-    <!-- <h1 class="lineup">
-      Want to know who's performing?<br />
-      <RouterLink to="/reunionartists">CHECK THE LINEUP</RouterLink><br />
-    </h1> -->
-    <h1 class="tickets">
-      Ready to buy tickets?‍<br />
-      <RouterLink to="/reuniontickets">BUY TICKETS</RouterLink>
-    </h1>
-    <h1 class="contact">
-      Have something to say?<br />
-      <RouterLink to="/reunioncontact">CONTACT US</RouterLink><br />
-    </h1>
+    <RouterLink to="/reunionapplication" class="link">
+      <h1 class="volunteer">
+        <span style="color: white">Want to volunteer?‍</span><br />
+        APPLY NOW
+      </h1>
+    </RouterLink>
+
+    <RouterLink to="/reuniontickets">
+      <h1 class="tickets">
+        <span style="color: white">Ready to buy tickets?‍</span><br />
+        BUY TICKETS
+      </h1>
+    </RouterLink>
   </div>
 </template>
 
 <style scoped>
+/* CSS Reset */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Arial, sans-serif;
+  line-height: 1.6;
+}
 .call-to-action {
   text-align: center;
   border: 3px solid #f0f0f0;
   border-radius: 30px;
-  margin-top: 1rem;
+  /* margin-top: 1rem; */
 }
 h1 {
   border: 1px solid rgba(121, 188, 255, 0.25);
-  padding: 1rem;
-  margin: 1rem;
   box-shadow: inset 0 0 50px rgba(121, 188, 255, 0.25);
+  padding: 1rem;
+  margin: 0.5rem;
 }
+/* top corners rounded */
 .volunteer,
 .lineup {
   border-top-left-radius: 20px;
   border-top-right-radius: 20px;
 }
 
+/* bottom corners rounded */
+.tickets,
 .contact {
   border-bottom-left-radius: 20px;
   border-bottom-right-radius: 20px;
