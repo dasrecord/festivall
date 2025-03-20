5b1afd50d66f30604e73f357049e352f7f759c16
Prasenjit Das <prasun.das.89@gmail.com>
2025-03-19 22:47:05 -0600

    feat: update volunteer instructions layout to enhance visual appeal and responsiveness

--- src/views/ReunionVolunteerInstructionsView.vue
+++ src/views/ReunionVolunteerInstructionsView.vue
@@ -16,68 +16,116 @@
         safely commit to.
       </h3>
       <div class="instructions">
-        <div class="team">
-          <button @click="router.push('/reunion-volunteer-signup')" class="volunteer-button">
-            ALL TEAMS SIGNUP
-          </button>
-          <p>
-            By selecting this option, you will be able to sign up for multiple teams and shifts
-            during the event. This option is ideal for volunteers who are flexible and willing to
-            help out in various capacities.
-          </p>
+        <div class="crew">
+          <img
+            :src="volunteer_icon"
+            alt="Volunteer Icon"
+            class="volunteer-icon"
+            style="width: 64px"
+          />
+          <div>
+            <button @click="router.push('/reunion-volunteer-signup')" class="volunteer-button">
+              ALL TEAMS SIGNUP
+            </button>
+            <p>
+              By selecting this option, you will be able to sign up for multiple teams and shifts
+              during the event. This option is ideal for volunteers who are flexible and willing to
+              help out in various capacities.
+            </p>
+          </div>
         </div>
-        <div class="team">
-          <button @click="router.push('/reunion-frontgate-signup')" class="volunteer-button">
-            FRONT GATE SIGNUP [Minimum One 4-Hour Shift Per Day]
-          </button>
-          <p>
-            Front Gate Volunteers are responsible for welcoming attendees, checking tickets, and
-            providing information about the event. They are the first point of contact for attendees
-            and play a crucial role in ensuring a positive experience for everyone.
-          </p>
+        <div class="crew">
+          <img
+            :src="frontgate_icon"
+            alt="Front Gate Icon"
+            class="volunteer-icon"
+            style="width: 64px"
+          />
+          <div>
+            <button @click="router.push('/reunion-frontgate-signup')" class="volunteer-button">
+              FRONT GATE SIGNUP [Minimum One 4-Hour Shift Per Day]
+            </button>
+            <p>
+              Front Gate Volunteers are responsible for welcoming attendees, checking tickets, and
+              providing information about the event. They are the first point of contact for
+              attendees and play a crucial role in ensuring a positive experience for everyone.
+            </p>
+          </div>
         </div>
-        <div class="team">
-          <button @click="router.push('/reunion-foodteam-signup')" class="volunteer-button">
-            FOOD TEAM SIGNUP [Minimum One 4-Hour Shift Per Day]
-          </button>
-          <p>
-            Food Team Volunteers are responsible for assisting with food preparation, serving, and
-            cleanup. They help ensure that attendees have access to food and refreshments during the
-            event and maintain cleanliness and hygiene in the food service area.
-          </p>
+        <div class="crew">
+          <img
+            :src="foodteam_icon"
+            alt="Food Team Icon"
+            class="volunteer-icon"
+            style="width: 64px"
+          />
+          <div>
+            <button @click="router.push('/reunion-foodteam-signup')" class="volunteer-button">
+              FOOD TEAM SIGNUP [Minimum One 4-Hour Shift Per Day]
+            </button>
+            <p>
+              Food Team Volunteers are responsible for assisting with food preparation, serving, and
+              cleanup. They help ensure that attendees have access to food and refreshments during
+              the event and maintain cleanliness and hygiene in the food service area.
+            </p>
+          </div>
         </div>
-        <div class="team">
-          <button @click="router.push('/reunion-setupcrew-signup')" class="volunteer-button">
-            SETUP CREW SIGNUP [Minimum One 8-Hour Shift PreShow]
-          </button>
-          <p>
-            Setup Crew Volunteers are responsible for setting up equipment, decorations, and other
-            items needed for the event. They help create a welcoming and festive atmosphere for
-            attendees and ensure that the event space is ready for the activities and programs
-            planned.
-          </p>
+        <div class="crew">
+          <img
+            :src="setupcrew_icon"
+            alt="Setup Crew Icon"
+            class="volunteer-icon"
+            style="width: 64px"
+          />
+          <div>
+            <button @click="router.push('/reunion-setupcrew-signup')" class="volunteer-button">
+              SETUP CREW SIGNUP [Minimum One 8-Hour Shift PreShow]
+            </button>
+            <p>
+              Setup Crew Volunteers are responsible for setting up equipment, decorations, and other
+              items needed for the event. They help create a welcoming and festive atmosphere for
+              attendees and ensure that the event space is ready for the activities and programs
+              planned.
+            </p>
+          </div>
         </div>
-        <div class="team">
-          <button @click="router.push('/reunion-cleanupcrew-signup')" class="volunteer-button">
-            CLEANUP CREW SIGNUP [Minimum One 8-Hour Shift PostShow]
-          </button>
-          <p>
-            Cleanup Crew Volunteers are responsible for clearing and cleaning the event space after
-            the event. They help pack up equipment, decorations, and other items, and ensure that
-            the venue is left in good condition. Cleanup Crew Volunteers play a vital role in
-            wrapping up the event smoothly and efficiently.
-          </p>
+        <div class="crew">
+          <img
+            :src="cleanupcrew_icon"
+            alt="Cleanup Crew Icon"
+            class="volunteer-icon"
+            style="width: 64px"
+          />
+          <div>
+            <button @click="router.push('/reunion-cleanupcrew-signup')" class="volunteer-button">
+              CLEANUP CREW SIGNUP [Minimum One 8-Hour Shift PostShow]
+            </button>
+            <p>
+              Cleanup Crew Volunteers are responsible for clearing and cleaning the event space
+              after the event. They help pack up equipment, decorations, and other items, and ensure
+              that the venue is left in good condition. Cleanup Crew Volunteers play a vital role in
+              wrapping up the event smoothly and efficiently.
+            </p>
+          </div>
         </div>
-        <div class="team">
-          <button @click="router.push('/reunion-stagecrew-signup')" class="volunteer-button">
-            STAGE CREW SIGNUP [Minimum One 4-Hour Shift Per Day]
-          </button>
-          <p>
-            Stage Crew Volunteers are responsible for assisting with stage setup, sound equipment,
-            lighting, and other technical aspects of the event. They help ensure that performers and
-            presenters have the necessary support and equipment for their performances and
-            presentations.
-          </p>
+        <div class="crew">
+          <img
+            :src="stagecrew_icon"
+            alt="Stage Crew Icon"
+            class="volunteer-icon"
+            style="width: 64px"
+          />
+          <div>
+            <button @click="router.push('/reunion-stagecrew-signup')" class="volunteer-button">
+              STAGE CREW SIGNUP [Minimum One 4-Hour Shift Per Day]
+            </button>
+            <p>
+              Stage Crew Volunteers are responsible for assisting with stage setup, sound equipment,
+              lighting, and other technical aspects of the event. They help ensure that performers
+              and presenters have the necessary support and equipment for their performances and
+              presentations.
+            </p>
+          </div>
         </div>
       </div>
       <h3>
@@ -92,6 +140,14 @@
 import frog_image from '@/assets/images/frog.png'
 import reunion_emblem from '../assets/images/reunion_emblem_white.png'
 import { useRouter } from 'vue-router'
+
+import volunteer_icon from '@/assets/images/icons/volunteer.png'
+import frontgate_icon from '@/assets/images/icons/front_gate.png'
+import foodteam_icon from '@/assets/images/icons/meals.png'
+import setupcrew_icon from '@/assets/images/icons/setup_crew.png'
+import cleanupcrew_icon from '@/assets/images/icons/cleanup_crew.png'
+import stagecrew_icon from '@/assets/images/icons/stage_crew.png'
+
 const router = useRouter()
 </script>
 
@@ -129,10 +185,20 @@ const router = useRouter()
   margin-top: 1rem;
 }
 
-.team {
-  margin-bottom: 1rem;
+.crew {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-top: 1rem;
   border: 1px solid var(--reunion-frog-green);
   padding: 1rem;
+  border-radius: 10px;
+}
+
+.crew img {
+  width: 64px;
+  height: auto;
+  margin: 0 1rem;
 }
 
 h2 {
